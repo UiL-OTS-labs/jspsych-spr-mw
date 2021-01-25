9d1e56785938de29654bd31545d9372c526f61a6
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2021-01-25 16:19:01 +0100

    Fix some comments, hence improve docs

--- globals.js
+++ globals.js
@@ -1,21 +1,21 @@
 
 // IMPORTANT
 // Access key, this must be modified.
-// You get this id when you have requested an experiment on
+// You will get this id when you have requested an experiment on
 // the data storage server.
-// If you do not fill out a valid key, the participants
+// If you do not fill out a valid key, the participant's
 // browser will not be able to upload the data to the server.
 // Replace this by a PERFECT COPY of the key from the data server.
 const ACCESS_KEY = '00000000-0000-0000-0000-000000000000';
 
 // This defines the dimensions of the canvas on which
 // the sentences are drawn. Keep in mind, that you'll exclude
-// participants with a low resolutions when you set this to
+// participants with a low screen resolution when you set this too
 // high.
-const MIN_WIDTH =  1000;
+const MIN_WIDTH = 1000;
 const MIN_HEIGHT = 600;
 
-// The ISI will be added after each trial/stimulus 
+// The ISI will be added after each trial/stimulus
 const ISI = 500; //ms
 
 // Fragments of text to display on buttons
@@ -28,18 +28,18 @@ const FALSE_BUTTON_TEXT = "false";
 // Duration of the fixation cross.
 const FIX_DUR = 500;
 
-// The duration in ms for howlong the finished instruction
+// The duration in ms for how long the finished instruction
 // is on screen.
 const FINISH_TEXT_DUR = 3000;
 
-// If no grouping character is selected or null as in this example
-// every word is a group of its own. And no 
-// Grouping character if none sentences are split on whitespace
+// If no grouping character is selected or if it is null as in this example
+// every word is a group of its own: sentences are split on whitespace.
 // each word will be a one word group
 const GROUPING_STRING = null;
-// Or create groups based on a splitting string
-// Create groups based on a "/" Notice every occurence
-// of a "/" will be used as group and will be removed from
-// the stimulus
+// Or create word groups based on a splitting string
+// Create groups using a "/". Note that every occurrence
+// of a "/" will lead to presentation as a word group and the a "/" itself
+// will not be displayed in the stimulus
 //const GROUPING_STRING = "/";
+
 
