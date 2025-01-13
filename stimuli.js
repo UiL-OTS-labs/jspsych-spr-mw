2fdd933ff64321b74737639983b2b237d7096144
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2025-01-13 10:50:06 +0100

    Update stimuli to current spr stimulus syntax

--- stimuli.js
+++ stimuli.js
@@ -23,16 +23,24 @@ const PRACTICE_ITEMS = [
     {
         id : 1,
         item_type : PRAC,
-
-        stimulus :                                                // Single "/" delimit boundaries between words presented
-                                                                  // together. Boundaries must be activated by setting "/" as
-                                                                  // GROUPING_STRING in globals.js. The default is null
-                                                                  // which means every word is a group of in its own.
-                                                                  // If the grouping string isn't set or null,
-                                                                  // the "/" will be displayed instead of used for grouping.
-            "The teacher took the car instead of the express"   + // The '+' adds strings together or concatenates them.
-            "train \ndue to the previously announced public\n"  + // A "\n" makes the string jump to the next line.
-            "transport strike.\n",                                // use a comma here.
+        //Every word, space "enter" between a pair of {{ and }} is presented
+        //together as one group of words. If a group starts with a {{#, the reaction
+        //will be recorded. Don't put any characters outside of the {{}}} characters
+        //as the grammar for shaping the self paced reading doesn't like that.
+        //
+        //You can go to the next line in this file by adding (+) to strings
+        //together.
+        //
+        //You are allowed to present words in html style bold and italic tags
+        //You can use bold and italic together, but an entire
+        //<b>this is <i>correct</i></b>,
+        //<b><i>but<b/> this isn't</i> as the entire italic phrase must be captured
+        //inside the bold tag. Open a new tag when the overlap partially.
+        //<b><i>but</i></b><i>this is</i> valid.
+        stimulus :
+            "{{The <b>teacher took <i>the car</i></b><i> instead</i> of }}{{the }}{{express }}"    + // string1
+            "{{train\n}}{{due to the previously announced public\n"                                 + // string2
+            "transport strike.}}", // use a comma here on the final line of your stimulus.             // final string
         question : "",                                            // An empty string means no question for this trial.
         qanswer : undefined                                       // We can't define a answer if there ain't no question.
     },
@@ -40,9 +48,9 @@ const PRACTICE_ITEMS = [
         id : 2,
         item_type : PRAC,
         stimulus :
-            "The researcher presented his most recent work\n"   +
-            "to the commission and obtained very positive\n"    +
-            "comments regarding the experimental design.\n"     ,
+            "{{The researcher presented his most recent work\n}}"   +
+            "{{to }}{{the }}{{commission }}{{and obtained very positive\n"    +
+            "comments regarding the experimental design.}}"     ,
         question : "The researcher presented old work.",
         qanswer : FALSE_BUTTON_TEXT                               // Use TRUE_BUTTON_TEXT if the answer is true,
                                                                   // FALSE_BUTTON_TEXT otherwise
@@ -50,17 +58,17 @@ const PRACTICE_ITEMS = [
 ];
 
 /*
- * In this list there is a stimulus, if a word or group of words starts with a
- * '#' it's reaction time will be recorded. So don't put any '#" elsewhere...
+ * In this list there is a stimulus, if a group of words starts with a
+ * '{{#' it's reaction time will be recorded.
  */
 const LIST_GROUP1 = [
     {
         id : 1,
         item_type : PASSIVE,
         stimulus :
-            "The guitarist was rejected by #the #attractive #and\n"     +
-            "#talented #singer #in #the #concert #hall #next #to\n"     +
-            "#the #Irish #pub.\n"                                       ,
+            "{{The guitarist was rejected by }}{{#the }}{{#attractive }}{{#and\n}}"                 +
+            "{{#talented }}{{#singer }}{{#in }}{{#the }}{{#concert }}{{#hall }}{{#next }}{{#to\n}}" +
+            "{{#the }}{{#Irish }}{{#pub.\n}}",
         question : "The singer was attractive.",
         qanswer : TRUE_BUTTON_TEXT
     },
@@ -68,9 +76,9 @@ const LIST_GROUP1 = [
         id : 2,
         item_type : ACTIVE,
         stimulus :
-            "The sculptor mugged #the #strange #and\n"                  +
-            "#temperamental #photographer #in #the #art #gallery\n"     +
-            "#next #to #the #book #shop.\n"                             ,
+            "{{The sculptor mugged }}{{#the }}{{#strange }}{{#and\n}}"                      +
+            "{{#temperamental }}{{#photographer }}{{#in }}{{#the }}{{#art }}{{#gallery\n}}" +
+            "{{#next }}{{#to }}{{#the }}{{#book }}{{#shop.\n}}"                             ,
         question : "",
         qanswer : undefined
     },
@@ -78,9 +86,9 @@ const LIST_GROUP1 = [
         id : 3,
         item_type : FILLER,
         stimulus :
-            "The beautiful princess married her young and\n"            +
-            "handsome chauffeur and shocked the royal\n"                +
-            "family and the press.\n"                                   ,
+            "{{The beautiful princess married her young and\n}}"                +
+            "{{handsome }}{{chauffeur }}{{and }}{{shocked }}{{the royal\n"      +
+            "family and the press.\n}}",
         question : "The chauffeur was an old man.",
         qanswer : FALSE_BUTTON_TEXT
     },
@@ -88,9 +96,9 @@ const LIST_GROUP1 = [
         id : 4,
         item_type : FILLER,
         stimulus :
-            "The little girl did not play with her brother\n"           +
-            "in the colourful playground next to their weedy\n"         +
-            "garden.\n"                                                 ,
+            "{{The little girl did not play with her brother\n}}"                               +
+            "{{in }}{{the }}{{colourful }}{{playground }}{{next }}{{to }}{{their }}{{weedy\n}}" +
+            "{{garden.\n}}",
         question : "",
         qanswer : undefined
     }
@@ -105,9 +113,9 @@ const LIST_GROUP2 = [
         id : 1,
         item_type : ACTIVE,
         stimulus :
-            "The guitarist rejected #the #attractive #and\n"            +
-            "#talented #singer #in #the #concert #hall #next #to\n"     +
-            "#the #Irish #pub.\n"                                       ,
+            "{{The guitarist rejected }}{{#the }}{{#attractive }}{{#and\n}}"                        +
+            "{{#talented }}{{#singer }}{{#in }}{{#the }}{{#concert }}{{#hall }}{{#next }}{{#to\n}}" +
+            "{{#the }}{{#Irish }}{{#pub.\n}}",
         question : "The singer was attractive.",
         qanswer : TRUE_BUTTON_TEXT
     },
@@ -115,9 +123,9 @@ const LIST_GROUP2 = [
         id : 2,
         item_type : PASSIVE,
         stimulus :
-            "The sculptor was mugged by #the #strange #and\n"           +
-            "#temperamental #photographer #in #the #art #gallery\n"     +
-            "#next #to #the #book #shop.\n"                             ,
+            "{{The sculptor was mugged by }}{{#the }}{{#strange }}{{#and\n}}"               +
+            "{{#temperamental }}{{#photographer }}{{#in }}{{#the }}{{#art }}{{#gallery\n}}" +
+            "{{#next }}{{#to }}{{#the }}{{#book }}{{#shop.\n}}",
         question : "",
         qanswer : undefined
     },
@@ -125,9 +133,9 @@ const LIST_GROUP2 = [
         id : 3,
         item_type : FILLER,
         stimulus :
-            "The beautiful princess married her young and\n"            +
-            "handsome chauffeur and shocked the royal\n"                +
-            "family and the press.\n"                                   ,
+            "{{The beautiful princess married her young and\n}}"                    +
+            "{{handsome }}{{chauffeur }}{{and }}{{shocked }}{{the }}{{royal\n}}"    +
+            "{{family and the press.\n}}",
         question : "The chauffeur was an old man.",
         qanswer : FALSE_BUTTON_TEXT
     },
@@ -135,9 +143,9 @@ const LIST_GROUP2 = [
         id : 4,
         item_type : FILLER,
         stimulus :
-            "The little girl did not play with her brother\n"           +
-            "in the colourful playground next to their weedy\n"         +
-            "garden.\n"                                                 ,
+            "{{The little girl did not play with her brother\n}}"           +
+            "{{in }}{{the }}{{colourful }}{{playground }}{{next }}{{to their weedy\n"         +
+            "garden.\n}}"                                                 ,
         question : "",
         qanswer : undefined
     }
